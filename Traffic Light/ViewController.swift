e49b5eda7b5877f458bc52292c7bf92f69b38da1
Jayden Patterson <jpatt768@gmail.com>
2022-01-27 18:35:17 +0900

    Added more cycles to the switch statement

--- Traffic Light/ViewController.swift
+++ Traffic Light/ViewController.swift
@@ -17,24 +17,27 @@ class ViewController: UIViewController {
     
 //    Here I decided to use an Int counter to change the phases of the lights. Starting at 0 once the button is pressed 1 is added and moves us through the Switch Statement in the function.
     var count = 0
+    
     func changeClolour() {
         count += 1
         switch count {
-        case 1:
+        case 1, 4, 7, 10, 13:
             greenLight.tintColor = .green
             myLabel.text = "This is a green light. If this colour is displayed you may proceed over the line into the intersection."
             redLight.tintColor = .black
-        case 2:
+        case 2, 5, 8, 11, 14:
             yellowLight.tintColor = .orange
             myLabel.text = "This is an amber light. When this light is displayed it means to express caution as the light will change red very soon. If you haven't yet entered the intersection, please stop your vehicle at the line when you see this light."
             greenLight.tintColor = .black
-        case 3:
+        case 3, 6, 9, 12, 15:
             redLight.tintColor = .red
             myLabel.text = "This is a red light. This light means that you may not proceed into the intersection as there is traffic moving in another direction or pedestrians are crossing the road. Please do not cross the line until the light changes back to green."
             yellowLight.tintColor = .black
         default:
             count = 0
-        }
+    }
+        
+    
     }
     override func viewDidLoad() {
         super.viewDidLoad()
